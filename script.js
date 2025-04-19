3643ecb04a3df4f39b981867701a12e5ea564a52
mash9000 <vm.certo@gmail.com>
2025-04-19 22:57:33 +0700

    рандомное описание к имени

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
 "use strict";
-let a = 2,
-  b = 4342,
-  c = -43992;
-let D = b ** 2 - 4 * a * c;
-console.log(`Дискриминант равен ${D}`);
-if (D < 0) {
-  console.log("Корней нет");
-} else if (D == 0) {
-  let x = -b / (2 * a);
-  console.log(`Единственный корень: ${x.toFixed(1)}`);
-} else if (D > 0) {
-  let x1 = (-b + Math.sqrt(D)) / (2 * a);
-  let x2 = (-b - Math.sqrt(D)) / (2 * a);
-  console.log(`Два корня: ${x1.toFixed(1)} и ${x2.toFixed(1)}`);
+
+console.log(randomDesc('female', 'Ольга'));
+
+function solveTheQuadraticEquation(a, b, c) {
+    let D = b ** 2 - 4 * a * c;
+    if (D < 0) {
+        return null;
+    } else if (D == 0) {
+        return {x: -b / (2 * a)};
+    } else if (D > 0) {
+        return {
+            x1: (-b - Math.sqrt(D)) / (2 * a),
+            x2: (-b + Math.sqrt(D)) / (2 * a),
+        };
+    }
 }
 
-let htmlTxt = "<p>Какой-нибудь текст</p>";
-let cssTxt =
-  '<style>p{font:bold 30px "Arial";width:300px;margin:0 auto;}</style>';
+function randomDesc(gender, name) {
+    if (checkGender(gender) && checkName(name)) {
+        let descMale = [
+            "Ужасный",
+            "Весёлый",
+            "Отвратительный",
+            "Грустный",
+            "Тактичный",
+        ];
 
-let human = { name: "Ivan", dayOfBirth: 1, monthOfBirth: 3, yearOfBirth: 1998 };
-console.log(human);
-human['height'] = 183;
-console.log(human);
+        let descFemale = [
+            "Ужасная",
+            "Весёлая",
+            "Отвратительная",
+            "Грустная",
+            "Тактичная",
+        ];
 
-for(let key of Object.keys(human)){
-    console.log(`${key}=${human[key]}`);
+        if (gender == "male") {
+            return descMale[getRandomInt(descMale.length - 1)] + " " + name;
+        } else {
+            return descFemale[getRandomInt(descFemale.length - 1)] + " " + name;
+        }
+    } else {
+        alert("Пшёл нахуй!");
+    }
+}
+
+function checkGender(gender) {
+    return gender != null && (gender == "male" || gender == "female");
+}
+
+function checkName(name) {
+    return name != null;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
