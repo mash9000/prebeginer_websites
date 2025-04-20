d738064f2ac4e1428ffb697838904809daa3a0b8
mash9000 <vm.certo@gmail.com>
2025-04-20 11:12:32 +0700

    добавил расчёты через стролочный оператор

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 "use strict";
 
-console.log(calc(1, 3, 'multiplication'));
+console.log(typeof [1,3, 3,4]);
 
-function calc(value1, value2, operator) {
-    if (isNumber(value1) && isNumber(value2) && isOperator(operator)) {
-        switch (operator) {
-            case 'plus':
-                return value1 + value2;
-            case 'minus':
-                return value1 - value2;
-            case 'multiplication':
-                return value1 * value2;
-            case 'divide':
-                return value1 / value2;
+console.log(calculateTheResistanceCoefficient(65, 500, 32.32))
+
+function calculateTheResistanceCoefficient(Re, delta, d) {
+    if (isNumber(Re) && isNumber(delta) && isNumber(d)) {
+        const R1 = 2300;
+        const R2 = 500;
+        const R3 = 56;
+
+        let makeCalculation = (Re, d, delta) => 64 / Re;
+        if (Re < R1) {
+            return makeCalculation(Re);
+        } else if ((Re >= R1) && (Re <= (R3 * (d / delta)))) {
+            makeCalculation = Re => .316 / (Re ** .25);
+            return makeCalculation(Re);
+        } else if ((R3 * (d / delta) <= Re) && (Re <= R2 * (d / delta))) {
+            makeCalculation = (Re, d, delta) => .11 * ((68 / Re) + (delta / d)) ** .25;
+            return makeCalculation(Re, d, delta);
+        } else if (Re > R2 * (d / delta)) {
+            makeCalculation = (Re, d, delta) => .11 * (delta / d) ** .25;
+            return makeCalculation(Re, d, delta);
         }
     } else {
-        alert('Иди на хер!');
+        console.log('Пшёл нахр');
     }
 }
 
 function isNumber(value) {
     return (typeof value) == 'number';
-}
-
-function isOperator(operator) {
-    return operator === 'plus' || operator === 'minus' || operator === 'multiplication' || operator === 'divide';
 }
